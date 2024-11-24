247473e4805b3662d9fc8123dbaa32977c519507
Heislae <raymond18oren@gmail.com>
2024-11-24 17:19:27 +0800

    Minor Changes to backend - Added some settings to the settings.py for setting up deployment

--- backend/project_files/settings.py
+++ backend/project_files/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s$8tfhkfs3)d&u79m#tpl7na^=2hqn4=bp@-ud70e^h)=^+y@1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["52.62.64.107", '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -140,6 +140,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_cdn",),
+    os.path.join(BASE_DIR, 'build', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
