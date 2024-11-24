b60cc32bb9bc2a45d5ca9c7bfd5f424e1ce1e937
Heislae <raymond18oren@gmail.com>
2024-11-24 17:32:02 +0800

    Fixed errors in backend

--- backend/project_files/urls.py
+++ backend/project_files/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('login_page.urls')),
     path('api/', include('admin_page.urls')),
     path('api/', include('chatbot_page.urls')),
-    path('', views.index, name="index"),
 ]
