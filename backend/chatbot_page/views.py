e1c3d6754cd60e4039f0aacad5bb5f9995f332fe
Heislae <raymond18oren@gmail.com>
2024-11-27 13:34:19 +0800

    token change

--- backend/chatbot_page/views.py
+++ backend/chatbot_page/views.py
@@ -65,7 +65,7 @@ def chatbot_view(request):
                 *conversation_history  # Send the entire conversation history
             ],
             model="llama-3.1-70b-versatile",
-            max_tokens=8000,
+            max_tokens=7500,
         )
 
         # Get chatbot response
